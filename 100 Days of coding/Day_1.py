9957829bc08081fa52074893e707f2e15b3be00b
Artur Cegiełka-Posłuszny <a.c.posluszny@gmail.com>
2024-02-27 19:36:47 +0100

    day 1

--- 100 Days of coding/Day_1.py
+++ 100 Days of coding/Day_1.py
@@ -1,1 +1,4 @@
-print("Hello World")
+print("Welcome in Band Name Generetor")
+city_name = input("What is the name of the city you grew up in? ")
+pet_name = input("What is your pet's name? ")
+print("Your band name could be " + city_name + " "+ pet_name)
