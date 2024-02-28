a6b6503b82b395d0dc8d3b71c0f82c08c86323d0
Artur Cegiełka-Posłuszny <a.c.posluszny@gmail.com>
2024-02-28 18:20:55 +0100

    drobna zmiana

--- 100 Days of coding/Day_2.py
+++ 100 Days of coding/Day_2.py
@@ -3,4 +3,4 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage would you like to give? 10, 12 or 15? ")) / 100
 amount_of_people = int(input("How many people to split the bill? "))
 each_person_pay = round((bill * (1 + tip)) / amount_of_people, 2)
-print(f"Each person should pay ${each_person_pay}")
+print(f"Each person should pay ${each_person_pay:.2f}")
